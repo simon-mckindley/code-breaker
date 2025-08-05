fa3f2cac01ef122de9c9b0aec3c16496de96e28c
SimonMck789 <S9406133@student.rmit.edu.au>
2025-08-05 12:45:11 +1000

    result pin animation

--- src/components/ResultContainer.jsx
+++ src/components/ResultContainer.jsx
@@ -2,9 +2,11 @@
 export default function ResultContainer({ result }) {
     return (
         <div className="guess-result">
-            {result.map((color, index) => (
-                <span key={index} className={`result ${color}`}></span>
-            ))}
+            {result.map((color, index) => {
+                const pegged = color ? true : false;
+
+                return <span key={index} data-peg={pegged} className={`result ${color}`}></span>
+            })}
         </div>
     );
 }
